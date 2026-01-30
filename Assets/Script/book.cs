7cffc18d14aa880d1293616c719aa150c1071e1b
annissa <annissabal@gmail.com>
2026-01-31 00:10:49 +0800

    chore: book flipping

--- Assets/Script/book.cs
+++ Assets/Script/book.cs
@@ -10,6 +10,9 @@ public class book : MonoBehaviour
     bool rotate = false;
     [SerializeField] GameObject backButton;
     [SerializeField] GameObject forwardButton;
+    public bool IsTurning => rotate;
+    public int OpenPage => Mathf.Clamp(index + 1, 0, pages.Count);
+    public int CurrentPageIndex => index;
 
     private void Start()
     {
@@ -18,42 +21,44 @@ public class book : MonoBehaviour
 
     public void InitialState()
     {
-        for (int i=0; i<pages.Count; i++)
+        for (int i = 0; i < pages.Count; i++)
         {
-            pages[i].transform.rotation=Quaternion.identity;
+            pages[i].transform.rotation = Quaternion.identity;
         }
         pages[0].SetAsLastSibling();
         backButton.SetActive(false);
-
     }
 
     public void RotateForward()
     {
         if (rotate == true) { return; }
+        if (index >= pages.Count - 1) { return; } // SAFETY CHECK: Don't go past last page
+
         index++;
-        float angle = 180; //in order to rotate the page forward, you need to set the rotation by 180 degrees around the y axis
+        float angle = 180;
         ForwardButtonActions();
         pages[index].SetAsLastSibling();
         StartCoroutine(Rotate(angle, true));
-
     }
 
     public void ForwardButtonActions()
     {
         if (backButton.activeInHierarchy == false)
         {
-            backButton.SetActive(true); //every time we turn the page forward, the back button should be activated
+            backButton.SetActive(true);
         }
         if (index == pages.Count - 1)
         {
-            forwardButton.SetActive(false); //if the page is last then we turn off the forward button
+            forwardButton.SetActive(false);
         }
     }
 
     public void RotateBack()
     {
         if (rotate == true) { return; }
-        float angle = 0; //in order to rotate the page back, you need to set the rotation to 0 degrees around the y axis
+        if (index < 0) { return; } // SAFETY CHECK: Don't go before first page
+
+        float angle = 0;
         pages[index].SetAsLastSibling();
         BackButtonActions();
         StartCoroutine(Rotate(angle, false));
@@ -63,39 +68,39 @@ public class book : MonoBehaviour
     {
         if (forwardButton.activeInHierarchy == false)
         {
-            forwardButton.SetActive(true); //every time we turn the page back, the forward button should be activated
+            forwardButton.SetActive(true);
         }
         if (index - 1 == -1)
         {
-            backButton.SetActive(false); //if the page is first then we turn off the back button
+            backButton.SetActive(false);
         }
     }
 
     IEnumerator Rotate(float angle, bool forward)
     {
         float value = 0f;
+        rotate = true;
+
+        // 1. HIDE EVERYTHING immediately when rotation starts
+        BroadcastMessage("SetContentVisible", false, SendMessageOptions.DontRequireReceiver);
+
         while (true)
         {
-            rotate = true;
             Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
             value += Time.deltaTime * pageSpeed;
-            pages[index].rotation = Quaternion.Slerp(pages[index].rotation, targetRotation, value); //smoothly turn the page
-            float angle1 = Quaternion.Angle(pages[index].rotation, targetRotation); //calculate the angle between the given angle of rotation and the current angle of rotation
+            pages[index].rotation = Quaternion.Slerp(pages[index].rotation, targetRotation, value);
+
+            float angle1 = Quaternion.Angle(pages[index].rotation, targetRotation);
             if (angle1 < 0.1f)
             {
-                if (forward == false)
-                {
-                    index--;
-                }
+                if (forward == false) { index--; }
                 rotate = false;
                 break;
-
             }
             yield return null;
-
         }
+
+        // 2. SHOW ONLY the correct content when rotation ends
+        BroadcastMessage("SetContentVisible", true, SendMessageOptions.DontRequireReceiver);
     }
-
-
-
 }
