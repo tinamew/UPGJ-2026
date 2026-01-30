1acb10b695c93cfb10d3dfe52c17c22256172a27
tinamew <pureefire@gmail.com>
2026-01-30 23:24:57 +0800

    Intro Cutscene Added

---
+++ Assets/Script/Cutscene Manager/CutsceneDialogueAnimator.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+
+public class CutsceneDialogueAnimator : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public string[] lines;
+
+    public void SetLine(int index)
+    {
+        Debug.Log("Signal fired: " + index);
+        if (index < 0 || index >= lines.Length) return;
+        text.text = lines[index];
+    }
+}
+
